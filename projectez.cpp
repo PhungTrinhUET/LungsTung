33978f176543febfd6c285ff1fb3a6bfb2ba2aae
PhungTrinhUET <phungtruongtrinhuet@gmail.com>
2023-03-25 12:08:09 +0700

    first commit

---
+++ projectez.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int quant;
+    int choice;
+    //Quantity 
+    int Qroom=0 , Qpasta=0, Qburger =0, Qnoodles =0, Qshake=0, Qchicken=0;
+    //Food items sold
+    int Srooms=0, Spasta=0, Sburger=0, Snoodles=0, Sshake=0, Schicken=0;
+    //Total proce of items
+    int Total_rooms=0, Total_pasta=0, Total_burger=0, Total_noodles=0, Total_shake=0, Total_chicken=0;
+
+
+
+    cout << "\n\t Quantity of items we have";
+    cout << "Rooms avaliabe : ";
+    cin >> Qroom;
+    cout << "\n Quantity of pasta : ";
+    cin >> Qpasta;
+    cout <<"\n Quantity of burger : ";
+    cin >> Qburger;
+    cout <<"\n Quantity of noodles : ";
+    cin >> Qnoodles;
+    cout << "\n Quantity of shake : ";
+    cin >> Qshake;
+    cout <<"\n Quantity of chicken : ";
+    cin >> Qchicken;
+
+
+    cout << "\n\t\t\t Please select from the menu options ";
+    cout << "\n\n1) Rooms ";
+    cout <<"\n2) Pasta";
+    cout <<"\n3) Burger";
+    cout <<"\n4) Noodles";
+    cout <<"\n5) shake";
+    cout <<"\n6 chicken ";
+    cout <<"\n7) Information regarding sales and collection  ";
+    cout <<"\n8) Exit ";
+    
+    cout <<"\n Please Enter your choice ! ";
+    cin >> choice;
+    
+    switch (choice)
+    {
+        case 1:
+        
+    }
+}
